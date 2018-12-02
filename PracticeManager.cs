de7d334e4f26bfc20b02a162e92ce39f9d851ea9
sigmasin <tksstepan@gmail.com>
2018-12-02 09:20:41 -0700

    initial version

---
+++ PracticeManager.cs
@@ -0,0 +1,390 @@
+using Core;
+using System;
+using System.IO;
+using Game;
+using UnityEngine;
+
+public static class PracticeManager
+{
+
+	public static void Initialize()
+	{
+		PracticeManager.MessageProvider = (PracticeMessageProvider)ScriptableObject.CreateInstance(typeof(PracticeMessageProvider));
+		PracticeManager.StartPosition = new Vector2(189f, -219.5f);
+		PracticeManager.EndPosition = new Vector2(138f, -218f);
+		PracticeManager.EndType = 4;
+		PracticeManager.EndReadable = "left";
+		PracticeManager.MessageInQueue = 0;
+		PracticeManager.Message = "";
+		PracticeManager.ResetValues();
+		PracticeManager.ParseSessionFile();
+	}
+
+	public static void ResetValues()
+	{
+		PracticeManager.FrameCount = Int32.MaxValue;
+		PracticeManager.FrameCountSession = Int32.MaxValue;
+		PracticeManager.FrameCountAll = Int32.MaxValue;
+		PracticeManager.Running = false;
+		PracticeManager.Countdown = -1;
+		if(Characters.Sein) 
+		{
+			Characters.Sein.Mortality.DamageReciever.IsImmortal = false;
+			GameController.Instance.LockInput = false;
+		}
+	}
+
+	public static void WriteFile()
+	{
+		StreamWriter streamWriter = new StreamWriter("PracticeSession.txt");
+		streamWriter.WriteLine("Start: " + PracticeManager.StartPosition.x.ToString() + ", " + PracticeManager.StartPosition.y.ToString());
+		streamWriter.WriteLine("End: " + PracticeManager.EndPosition.x.ToString() + ", " + PracticeManager.EndPosition.y.ToString() + ", " + PracticeManager.EndType.ToString());
+		streamWriter.WriteLine("Best: " + PracticeManager.FrameCountAll.ToString());
+		streamWriter.Flush();
+		streamWriter.Close();
+	}
+
+	public static void ParseSessionFile() 
+	{
+		if (!File.Exists("PracticeSession.txt"))
+		{
+			PracticeManager.WriteFile();
+		}
+		try
+		{
+			string[] lines = File.ReadAllLines("PracticeSession.txt");
+			string[] start = lines[0].Split(':')[1].Split(',');
+			PracticeManager.StartPosition = new Vector2(float.Parse(start[0]), float.Parse(start[1]));
+			string[] end = lines[1].Split(':')[1].Split(',');
+			PracticeManager.EndPosition = new Vector2(float.Parse(end[0]), float.Parse(end[1]));
+			PracticeManager.EndType = int.Parse(end[2]);
+			PracticeManager.SetEndReadable();
+			string[] best = lines[2].Split(':');
+			PracticeManager.FrameCountAll = int.Parse(best[1]);
+		}
+		catch (Exception)
+		{
+			PracticeManager.ShowMessage("Error parsing session file");
+		}
+
+	}
+
+	public static void Update()
+	{
+		PracticeManager.UpdateMessages();
+		if (PracticeManager.Countdown >= 0)
+		{
+			PracticeManager.Countdown--;
+			if (Characters.Sein)
+			{
+				Characters.Sein.Position = PracticeManager.StartPosition;
+			}
+			if (PracticeManager.Countdown == 0)
+			{
+				PracticeManager.Start();
+			}
+		}
+		if (PracticeManager.Running)
+		{
+			if(PracticeManager.FrameCount == 1)
+			{
+				foreach (Core.Input.InputButtonProcessor button in Core.Input.Buttons)
+					{
+						if(button != Core.Input.AnyStart)
+						{
+							button.IsPressed = false;
+							button.WasPressed = false;
+						}
+					}
+			}
+			PracticeManager.FrameCount++;
+			PracticeManager.CheckEnd();
+		}
+		if(MoonInput.GetKey(KeyCode.LeftAlt) || MoonInput.GetKey(KeyCode.RightAlt))
+		{
+			if(MoonInput.GetKeyDown(KeyCode.Keypad5))
+			{
+				PracticeManager.SetStart();
+			}
+			if(MoonInput.GetKeyDown(KeyCode.R))
+			{
+				PracticeManager.QueueStart();
+			}
+			if(MoonInput.GetKeyDown(KeyCode.L))
+			{
+				PracticeManager.Initialize();
+			}
+			if(MoonInput.GetKeyDown(KeyCode.I))
+			{
+				PracticeManager.ShowPositionInfo();
+			}
+			if(MoonInput.GetKeyDown(KeyCode.T))
+			{
+				PracticeManager.MessageInQueue = 2;
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad1))
+			{
+				PracticeManager.SetEnd(1);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad2))
+			{
+				PracticeManager.SetEnd(2);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad3))
+			{
+				PracticeManager.SetEnd(3);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad4))
+			{
+				PracticeManager.SetEnd(4);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad6))
+			{
+				PracticeManager.SetEnd(6);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad7))
+			{
+				PracticeManager.SetEnd(7);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad8))
+			{
+				PracticeManager.SetEnd(8);
+			}
+			if(MoonInput.GetKeyDown(KeyCode.Keypad9))
+			{
+				PracticeManager.SetEnd(9);
+			}
+		}
+	}
+
+	public static void SetStart()
+	{
+		PracticeManager.ResetValues();
+		PracticeManager.StartPosition = new Vector2(Characters.Sein.Position.x, Characters.Sein.Position.y);
+		int slot = SaveSlotsManager.CurrentSlotIndex;
+		SaveSlotsManager.CurrentSlotIndex = 49;
+		SaveSlotsManager.BackupIndex = -1;
+		GameController.Instance.CreateCheckpoint();
+		GameController.Instance.SaveGameController.PerformSave();
+		SaveSlotsManager.CurrentSlotIndex = slot;
+		PracticeManager.WriteFile();
+		PracticeManager.ShowMessage("Start set: " + PracticeManager.StartPosition.ToString(), 5f);
+	}
+
+	public static void SetEnd(int type)
+	{
+		PracticeManager.ResetValues();
+		PracticeManager.EndPosition = new Vector2(Characters.Sein.Position.x, Characters.Sein.Position.y);
+		PracticeManager.EndType = type;
+		PracticeManager.SetEndReadable();
+		PracticeManager.WriteFile();
+		PracticeManager.ShowMessage("End set: " + PracticeManager.EndPosition.ToString() + " " + PracticeManager.EndReadable);
+	}
+
+	public static void SetEndReadable()
+	{
+		switch(PracticeManager.EndType)
+		{
+			case 1:
+				PracticeManager.EndReadable = "down-left";
+				break;
+			case 2:
+				PracticeManager.EndReadable = "down";
+				break;
+			case 3:
+				PracticeManager.EndReadable = "down-right";
+				break;
+			case 4:
+				PracticeManager.EndReadable = "left";
+				break;
+			case 6:
+				PracticeManager.EndReadable = "right";
+				break;
+			case 7:
+				PracticeManager.EndReadable = "up-left";
+				break;
+			case 8:
+				PracticeManager.EndReadable = "up";
+				break;
+			case 9:
+				PracticeManager.EndReadable = "up-right";
+				break;
+			default:
+				PracticeManager.EndReadable = "ERROR: end direction unset";
+				break;
+		}
+	}
+
+	public static void QueueStart()
+	{
+		int currentSlotIndex = SaveSlotsManager.CurrentSlotIndex;
+		SaveSlotsManager.CurrentSlotIndex = 49;
+		SaveSlotsManager.BackupIndex = -1;
+		GameController.Instance.SaveGameController.PerformLoad();
+		SaveSlotsManager.CurrentSlotIndex = currentSlotIndex;
+		if (Characters.Sein)
+		{
+			Characters.Sein.Position = PracticeManager.StartPosition;
+			Characters.Sein.Mortality.DamageReciever.IsImmortal = true;
+		}
+		PracticeManager.ShowMessage("Ready...", 5f);
+		PracticeManager.Countdown = 60;
+	}
+
+	public static void Start()
+	{
+		int currentSlotIndex = SaveSlotsManager.CurrentSlotIndex;
+		SaveSlotsManager.CurrentSlotIndex = 49;
+		SaveSlotsManager.BackupIndex = -1;
+		GameController.Instance.SaveGameController.PerformLoad();
+		SaveSlotsManager.CurrentSlotIndex = currentSlotIndex;
+		if (Characters.Sein)
+		{
+			Characters.Sein.Position = PracticeManager.StartPosition;
+			Characters.Sein.Mortality.DamageReciever.IsImmortal = false;
+		}
+		PracticeManager.ShowMessage("$GO$", 1f);
+		PracticeManager.Running = true;
+		PracticeManager.FrameCount = -1;
+	}
+
+	public static void End()
+	{
+		PracticeManager.Running = false;
+		if(PracticeManager.FrameCount < PracticeManager.FrameCountSession)
+		{
+			PracticeManager.FrameCountSession = PracticeManager.FrameCount;
+		}
+		if(PracticeManager.FrameCount < PracticeManager.FrameCountAll)
+		{
+			PracticeManager.FrameCountAll = PracticeManager.FrameCount;
+			PracticeManager.WriteFile();
+		}
+		PracticeManager.ShowFrameInfo();
+	}
+
+	public static void CheckEnd()
+	{
+		if (!Characters.Sein || Characters.Sein.Position.x < -2000f)
+		{
+			return;
+		}
+		switch(PracticeManager.EndType)
+		{
+			case 1:
+				if(Characters.Sein.Position.x < PracticeManager.EndPosition.x && Characters.Sein.Position.y < PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 2:
+				if(Characters.Sein.Position.y < PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 3:
+				if(Characters.Sein.Position.x > PracticeManager.EndPosition.x && Characters.Sein.Position.y < PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 4:
+				if(Characters.Sein.Position.x < PracticeManager.EndPosition.x)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 6:
+				if(Characters.Sein.Position.x > PracticeManager.EndPosition.x)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 7:
+				if(Characters.Sein.Position.x < PracticeManager.EndPosition.x && Characters.Sein.Position.y > PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 8:
+				if(Characters.Sein.Position.y > PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			case 9:
+				if(Characters.Sein.Position.x > PracticeManager.EndPosition.x && Characters.Sein.Position.y > PracticeManager.EndPosition.y)
+				{
+					PracticeManager.End();
+				}
+				break;
+			default:
+				PracticeManager.End();
+				break;
+
+		}
+	}
+
+	public static void ShowMessage(string message, float time = 5f)
+	{
+		PracticeManager.Message = message;
+		PracticeManager.MessageTime = time;
+		PracticeManager.MessageInQueue = 2;
+	}
+
+	public static void UpdateMessages()
+	{
+		if(PracticeManager.MessageInQueue > 0)
+		{
+			PracticeManager.MessageProvider.SetMessage(PracticeManager.Message);
+			UI.Hints.Show(PracticeManager.MessageProvider, HintLayer.GameSaved, PracticeManager.MessageTime);
+			PracticeManager.MessageInQueue--;
+		}
+	}
+
+	public static void ShowPositionInfo()
+	{
+		UI.SeinUI.ShowUI = true;
+		SeinUI.DebugHideUI = false;
+		PracticeManager.ShowMessage("Start Position: " + PracticeManager.StartPosition.ToString() + "\n" +
+										"End Position: " + PracticeManager.EndPosition.ToString() + " " + PracticeManager.EndType.ToString() + "\n" +
+										"Current Position: " + new Vector2(Characters.Sein.Position.x, Characters.Sein.Position.y).ToString());
+	}
+
+	public static void ShowFrameInfo()
+	{
+		UI.SeinUI.ShowUI = true;
+		SeinUI.DebugHideUI = false;
+		PracticeManager.ShowMessage("Frames: " + PracticeManager.FrameCount.ToString() + "\n" +
+										"Session Best: " + PracticeManager.FrameCountSession.ToString() + "\n" +
+										"Overall Best: " + PracticeManager.FrameCountAll.ToString());
+
+	}
+
+	public static PracticeMessageProvider MessageProvider;
+
+	public static float MessageTime;
+
+	public static string Message;
+
+	public static int MessageInQueue;
+
+	public static Vector2 StartPosition;
+
+	public static Vector2 EndPosition;
+
+	public static int EndType;
+
+	public static string EndReadable;
+
+	public static int Countdown;
+
+	public static int FrameCount;
+
+	public static int FrameCountSession;
+
+	public static int FrameCountAll;
+
+	public static bool Running;
+}
---
+++ readme.txt
@@ -0,0 +1,50 @@
+The main idea behind this mod is to time sections by setting a start point with Alt+Numpad5, setting a desired end point, then using Alt+R to run the segment until you're satisfied.
+
+Here is a demo of some features: https://streamable.com/j6r67
+
+Full command list:
+
+Alt+Numpad5: Set start point (current Ori position)
+Alt+Numpad1: Set end area (down-left of current Ori position)
+Alt+Numpad2: Set end area (down)
+Alt+Numpad3: Set end area (down-right)
+Alt+Numpad4: Set end area (left)
+Alt+Numpad6: Set end area (right)
+Alt+Numpad7: Set end area (up-left)
+Alt+Numpad8: Set end area (up)
+Alt+Numpad9: Set end area (up-right)
+
+Alt+R: Start the run.
+Alt+L: Reload PracticeSession.txt
+Alt+I: Show current position, start point, end point, and end direction.
+Alt+T: Replay last message
+
+Notes:
+
+When Alt+Numpad5 is pressed, the game is saved in slot 50, then the slot is switched back to where it was before. Slot 50 saves will be overwritten without confirmation. The mod will not function correctly if you run while in slot 50, but running in any other slot will enable you to practice segments containing saves.
+
+The full set of actions that gets taken upon pressing Alt+R:
+-The save in slot 50 is loaded
+-A hint is printed: "Ready..."
+-Ori's position is locked to the start position for 1 second, Ori is invulnerable during this time
+-The save in slot 50 is loaded again
+-Ori's position is set to the start position
+-A hint is printed: "GO"
+-Frame counting begins
+-Any held inputs are forcibly released and repressed on the first frame of counting, thus you can buffer a dash, jump, bash, etc. at the beginning of the segment for more accurate timing. This feature has some quirks, but is functional -- for example, to buffer a cdash, you must press dash then press charge; pressing them together just gives a regular dash.
+
+UI will be toggled on upon reaching the end automatically to display the results.
+
+Upon setting a start point, end point, or beating the overall best for your start point/end point, PracticeSession.txt will be written. You could use this to share the same start/end points, but note that you'll also need to either send your slot 50 save over, or create a save in slot 50 in the area. Alt+R will set your position after loading, so just having the same skills and being in roughly the same place is good enough here.
+
+Starting a new game will turn on UI and print a hint that indicates that this mod is active to prevent invalid runs as a result of the recompiled game.
+
+The base dll for this mod is the nopause dll with menu commands, 50 slots, and a 0.5 bash deadzone.
+
+Modified classes:
+GameController
+NewGameAction
+
+Added classes:
+PracticeManager
+PracticeMessageProvider
